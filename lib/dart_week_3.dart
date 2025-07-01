95a1ea9e56b3d039618c07c9b8946c4aa1714461
Programmerstephen1 <166320110+Programmerstephen1@users.noreply.github.com>
2025-07-01 10:50:27 +0300

    Update dart_week_3.dart

--- lib/dart_week_3.dart
+++ lib/dart_week_3.dart
@@ -1,3 +1,24 @@
-int calculate() {
-  return 6 * 7;
+void main() {
+  // Encapsulation
+  var account = BankAccount("Stephen", 1000.0);
+  account.deposit(250.0);
+  account.withdraw(500.0);
+  print("Final Balance: \$${account.balance}");
+
+  print("\n--- Inheritance ---");
+  var student = Student("Stephen", 20, "Imani Academy");
+  student.introduce();
+  student.study();
+
+  print("\n--- Polymorphism ---");
+  List<Animal> animals = [Dog(), Cat()];
+  for (var animal in animals) {
+    animal.makeSound();
+  }
+
+  print("\n--- Abstraction ---");
+  List<Shape> shapes = [Circle(), Square()];
+  for (var shape in shapes) {
+    shape.draw();
+  }
 }
